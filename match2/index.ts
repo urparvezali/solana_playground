f70eb667200a239633b5f913560f7fb4abdf3a54
Parvez Ali <urparvezali@gmail.com>
2025-02-15 19:49:19 +0600

    transaction and getting airdrop

--- match2/index.ts
+++ match2/index.ts
@@ -1,18 +1,51 @@
-import { clusterApiUrl, Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+import {
+	clusterApiUrl,
+	Connection,
+	Keypair,
+	LAMPORTS_PER_SOL,
+	sendAndConfirmTransaction,
+	SystemProgram,
+	Transaction
+} from "@solana/web3.js";
+
+const receiver = Keypair.generate();
 
 
-const addrs = new PublicKey('we1Aj41BoHsybq1po6uEDnj2k1GjUUkWaUEa6Zs8etw');
+const sender = Keypair.generate();
 const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
 
-const signature = await connection.requestAirdrop(addrs, LAMPORTS_PER_SOL);
-const x = await connection.confirmTransaction(signature, "confirmed");
-console.log(x.value);
+const signature1 = await connection.requestAirdrop(sender.publicKey, LAMPORTS_PER_SOL);
+const x = await connection.confirmTransaction(signature1, "confirmed");
 
-
-
-const balance = await connection.getBalance(addrs);
-const balance_in_sol = balance / LAMPORTS_PER_SOL;
+let balance = await connection.getBalance(sender.publicKey);
+let balance_in_sol = balance / LAMPORTS_PER_SOL;
 
 console.log("Balance (lamports):", balance);
 console.log("Balance (SOL):", balance_in_sol);
+
+
+const transaction = new Transaction();
+const send_instruction = SystemProgram.transfer({
+	fromPubkey: sender.publicKey,
+	toPubkey: receiver.publicKey,
+	lamports: LAMPORTS_PER_SOL / 10,
+});
+transaction.add(send_instruction);
+
+const signature2 = await sendAndConfirmTransaction(connection, transaction, [sender]);
+
+
+balance = await connection.getBalance(sender.publicKey);
+balance_in_sol = balance / LAMPORTS_PER_SOL;
+
+console.log(signature2.toString());
+console.log("Balance (lamports):", balance);
+console.log("Balance (SOL):", balance_in_sol);
+
+// OUTPUT:
+// Balance(lamports): 1000000000
+// Balance(SOL): 1
+// 6649XotRWJunLZB9N67WJdzDxEYephfacELdE1j1YYBMo9oMv878386aUAwyCs9DzHuYtEmTMRR9aH5K5kjrNQeK
+// Balance(lamports): 899995000
+// Balance(SOL): 0.899995
